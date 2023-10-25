5611d50653293a9bc9941974e45070200b781995
David-Stodola <140344163+David-Stodola@users.noreply.github.com>
2023-10-24 22:24:35 -0300

    Acceso desde NavBAR
    Se arregló un error que tenia en el nav bar, los cuales no permitian acceder a los user.

---
+++ Proyecto-entrega/js/storage.js
@@ -0,0 +1,26 @@
+localStorage.setItem("nombre", 200);
+localStorage.setItem("currentUser", "Pedro Talio");
+
+const nombreGuardado = localStorage.getItem("nombre");
+
+console.log(JSON.parse(nombreGuardado));
+
+Swal.fire("Bienvenido", `Que bueno verte de nuevo ${nombreGuardado}`);
+
+// localStorage.removeItem("currentUser")
+
+// localStorage.clear()
+
+const users = {
+  fullname: "James Moore",
+  email: "james.moore@example.com",
+  id: "8",
+};
+
+//Cuando voy a guardar un objeto o array tengo que si o si convertirlo a una string
+localStorage.setItem("usuariosGuardados", JSON.stringify(users));
+
+// Cuando obtengo el dato lo tranformo a un objeto JS para poder trabajarlo como tal
+const temp = JSON.parse(localStorage.getItem("usuariosGuardados"));
+
+console.log(temp);
